25f3a3751d1a126d8bf76704aa009f7fe16c0bf5
Maulanawesome5 <maulanawesome5@gmail.com>
2021-12-14 02:29:45 +0700

    Mengubah isi file excel harga saham. Mengubah file 'Saham.py'

--- 07 - Modern Portfolio/Saham.py
+++ 07 - Modern Portfolio/Saham.py
@@ -18,12 +18,46 @@ SahamSMRA = pd.read_excel(DataHargaSMRA)
 
 # method untuk menampilkan harga saham
 def DisplayHargaSaham():
-    print(SahamAPLN, "\n")
-    print(SahamASRI, "\n")
-    print(SahamCTRA, "\n")
-    print(SahamPWON, "\n")
-    print(SahamSMRA, "\n")
+    print(15*"=", "APLN", 15*"=", "\n", SahamAPLN, "\n")
+    print(15*"=", "ASRI", 15*"=", "\n", SahamASRI, "\n")
+    print(15*"=", "CTRA", 15*"=", "\n", SahamCTRA, "\n")
+    print(15*"=", "PWON", 15*"=", "\n", SahamPWON, "\n")
+    print(15*"=", "SMRA", 15*"=", "\n", SahamSMRA, "\n")
 
+# Memilih tabel closing
+hargaClosingAPLN = SahamAPLN['Close']
+hargaClosingASRI = SahamASRI['Close']
+hargaClosingCTRA = SahamCTRA['Close']
+hargaClosingPWON = SahamPWON['Close']
+hargaClosingSMRA = SahamSMRA['Close']
+
+# Log persentase perubahan
+APLN = hargaClosingAPLN.pct_change().apply(lambda x: np.log(1+x))
+ASRI = hargaClosingASRI.pct_change().apply(lambda x: np.log(1+x))
+CTRA = hargaClosingCTRA.pct_change().apply(lambda x: np.log(1+x))
+PWON = hargaClosingPWON.pct_change().apply(lambda x: np.log(1+x))
+SMRA = hargaClosingSMRA.pct_change().apply(lambda x: np.log(1+x))
+
+# Variance
+variance_apln = APLN.var()
+variance_asri = ASRI.var()
+variance_ctra = CTRA.var()
+variance_pwon = PWON.var()
+variance_smra = SMRA.var()
+
+# Volatility
+volatil_apln = np.sqrt(variance_apln * 250)
+volatil_asri = np.sqrt(variance_asri * 250)
+volatil_ctra = np.sqrt(variance_ctra * 250)
+volatil_pwon = np.sqrt(variance_pwon * 250)
+volatil_smra = np.sqrt(variance_smra * 250)
+
+# Volatility kelima saham
+hargaClosingAPLN.pct_change().apply(lambda x: np.log(1+x)).std().apply(lambda x: x*np.sqrt(250)).plot(kind='bar')
+hargaClosingASRI.pct_change().apply(lambda x: np.log(1+x)).std().apply(lambda x: x*np.sqrt(250)).plot(kind='bar')
+hargaClosingCTRA.pct_change().apply(lambda x: np.log(1+x)).std().apply(lambda x: x*np.sqrt(250)).plot(kind='bar')
+hargaClosingPWON.pct_change().apply(lambda x: np.log(1+x)).std().apply(lambda x: x*np.sqrt(250)).plot(kind='bar')
+hargaClosingSMRA.pct_change().apply(lambda x: np.log(1+x)).std().apply(lambda x: x*np.sqrt(250)).plot(kind='bar')
 
 
 
