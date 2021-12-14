0ad2f39810dead9b864c24f79b83e10e0a783d4f
Maulanawesome5 <maulanawesome5@gmail.com>
2021-12-14 15:31:28 +0700

    Mengupdate sintaks program Test.py

--- 07 - Modern Portfolio/Test.py
+++ 07 - Modern Portfolio/Test.py
@@ -38,4 +38,32 @@ smra_volatile = np.sqrt(var_smra * 250)
 # Plotting Volatility kelima saham
 test.pct_change().apply(lambda x: np.log(1+x)).std().apply(lambda x: x*np.sqrt(250)).plot(kind='bar')
 
+# Calculating covariance
+test1 = test.pct_change().apply(lambda x: np.log(1+x))
+test1.head()
+
+# Covariance
+test1['APLN.JK'].cov(test1['ASRI.JK'])
+
+# Correlation
+test1['APLN.JK'].corr(test1['ASRI.JK'])
+
+# Expected Return
+test2 = test.pct_change().apply(lambda x: np.log(1+x))
+test2.head()
+
+# Define weight for allocation
+bobot = [0.2, 0.8, 0.5, 0.5, 8.0]
+alokasiBobot = test2.mean()
+print(alokasiBobot)
+
+# Total expected return
+expectedReturn = (alokasiBobot * bobot).sum()
+print(expectedReturn)
+
+# Building Optimal Risky portfolio
+
+
+
+
 
