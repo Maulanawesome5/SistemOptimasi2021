01da135035f8ac5b6e81598675f2077555a4db0f
maulanawesome5 <60890352+Maulanawesome5@users.noreply.github.com>
2021-12-20 19:41:32 +0700

    Mengubah test.py

--- 07 - Modern Portfolio/StockClass.py
+++ 07 - Modern Portfolio/StockClass.py
@@ -2,8 +2,15 @@ import os
 import glob
 import pandas as pd
 
-# Get data file name
+# Import data
 alamat_file = r'D:\LATIHAN PEMROGRAMAN\SISTEM OPTIMASI\DATA_HARGA_SAHAM'
 fileName = os.listdir(alamat_file)
-
 print(fileName)
+
+df = pd.DataFrame()
+
+for f in fileName:
+    data = pd.read_excel(f, 'Sheet1')
+    df = df.append(data)
+
+print(df)
--- 07 - Modern Portfolio/Test.py
+++ 07 - Modern Portfolio/Test.py
@@ -4,131 +4,51 @@ import pandas as pd
 from pandas_datareader import data
 import matplotlib.pyplot as plt
 
-# Read data
+# Membaca data harga saham dengan metode scrapping
 test = data.DataReader(
-    ['APLN.JK', 'ASRI.JK', 'CTRA.JK', 'PWON.JK', 'SMRA.JK'], 'yahoo',
+    ['APLN.JK', 'ASRI.JK'], 'yahoo',
     start = '2020/12/07',
     end = '2021/12/07')
 
-# Closing price
+# Harga closing
 test = test['Adj Close']
-test.head()
+print(test.head())
 
-# Log of percentage
-apln = test['APLN.JK'].pct_change().apply(lambda x: np.log(1+x))
-asri = test['ASRI.JK'].pct_change().apply(lambda x: np.log(1+x))
-ctra = test['CTRA.JK'].pct_change().apply(lambda x: np.log(1+x))
-pwon = test['PWON.JK'].pct_change().apply(lambda x: np.log(1+x))
-smra = test['SMRA.JK'].pct_change().apply(lambda x: np.log(1+x))
+# Menghitung persentase perubahan harga
+apln = test['APLN.JK'].pct_change().apply(lambda x: np.log(1 + x))
+asri = test['ASRI.JK'].pct_change().apply(lambda x: np.log(1 + x))
+print("\n", apln.head(), asri.head())
 
-# Variance
-var_apln = apln.var()
-var_asri = asri.var()
-var_ctra = ctra.var()
-var_pwon = pwon.var()
-var_smra = smra.var()
+# Menghitung varians harga saham
+varians_apln = apln.var()
+varians_asri = asri.var()
+print("\n",f"Varians APLN.JK = {varians_apln}, Varians ASRI.JK = {varians_asri} ")
 
-# Volatility
-apln_volatile = np.sqrt(var_apln * 250)
-asri_volatile = np.sqrt(var_asri * 250)
-ctra_volatile = np.sqrt(var_ctra * 250)
-pwon_volatile = np.sqrt(var_pwon * 250)
-smra_volatile = np.sqrt(var_smra * 250)
+# Menghitung volatilitas pergerakan saham
+apln_volatile = np.sqrt(varians_apln * 250)
+asri_volatile = np.sqrt(varians_asri * 250)
+print("\n",f"Volatilitas APLN.JK = {apln_volatile}, Volatilitas ASRI.JK = {asri_volatile} ")
 
-# Plotting Volatility kelima saham
-test.pct_change().apply(lambda x: np.log(1+x)).std().apply(lambda x: x*np.sqrt(250)).plot(kind='bar')
+# Menampilkan volatilitas kedua saham menjadi bentuk diagram batang
+test.pct_change().apply(
+    lambda x: np.log(1 + x)).std().apply(
+        lambda x: x*np.sqrt(250)).plot(kind='bar')
+# plt.show()
 
-# Calculating covariance
+# Persentase perubahan
 test1 = test.pct_change().apply(lambda x: np.log(1+x))
-test1.head()
+print(test1.head())
 
-# Covariance
+# Menghitung covariance kedua saham
 test1['APLN.JK'].cov(test1['ASRI.JK'])
+print("\n\tNilai Covariance antara APLN dengan ASRI \n",test1)
 
-# Correlation
+# Menghitung correlation antara kedua saham
 test1['APLN.JK'].corr(test1['ASRI.JK'])
-
-# Expected Return
-test2 = test.pct_change().apply(lambda x: np.log(1+x))
-test2.head()
-
-# Define weight for allocation
-bobot = [0.2, 0.8, 0.5, 0.5, 8.0]
-alokasiBobot = test2.mean()
-alokasiBobot
-
-# Total expected return
-expectedReturn = (alokasiBobot * bobot).sum()
-expectedReturn
-
-# Building Optimal Risky portfolio
-df = test
-df.head()
-
-# Covariance and Correlation matrix
-# Log of percentage change
-cov_matrix = df.pct_change().apply(lambda x: np.log(1+x)).cov()
-cov_matrix
-
-# Correlation kelima saham
-corr_matrix = df.pct_change().apply(lambda x: np.log(1+x)).corr()
-corr_matrix
+print("\n\tNilai Correlation antara APLN dengan ASRI \n", test1)
 
 
-# portfolio variance
-# randomly weighted portfolio's variance
-bobot = {'APLN.JK' : 0.2, 'ASRI.JK' : 0.8, 'CTRA.JK' : 0.5, 'PWON.JK' : 0.5, 'SMRA.JK' : 8.0}
-port_var = cov_matrix.mul(bobot, axis=0).mul(bobot, axis=1).sum().sum()
-port_var
 
-# portfolio expected return
-# yearly return for individual companies
-ind_er = df.resample('Y').last().pct_change().mean()
-ind_er
-
-# portfolio return
-bobot = [0.2, 0.8, 0.5, 0.5, 8.0]
-port_er = (bobot * ind_er).sum()
-print(port_er)
-
-# Volatility is given by the annual standard deviation. We multiply by 250 because there are 250 trading days/year.
-ann_sd = df.pct_change().apply(lambda x: np.log(1+x)).std().apply(lambda x: x*np.sqrt(250))
-print(ann_sd)
-
-
-assets = pd.concat([ind_er, ann_sd], axis=1) # Creating a table for visualising returns and volatility of assets
-assets.columns = ['Returns', 'Volatility']
-print(assets)
-
-p_ret = [] # Define an empty array for portfolio return
-p_vol = [] # Define an empty array for portfolio volatility
-p_weights = [] # Define an empty array for asset weight
-
-num_assets = len(df.columns)
-num_portfolios = 10000
-
-for portfolio in range(num_portfolios):
-    weights = np.random.random(num_assets)
-    weights = weights/np.sum(weights)
-    p_weights.append(weights)
-    returns = np.dot(weights, ind_er)
-    
-    p_ret.append(returns)
-    var = cov_matrix.mul(weights, axis=0).mul(weights, axis=1).sum().sum()# Portfolio Variance
-    sd = np.sqrt(var) # Daily standard deviation
-    ann_sd = sd*np.sqrt(250) # Annual standard deviation = volatility
-    p_vol.append(ann_sd)
-
-data = {'Returns':p_ret, 'Volatility':p_vol}
-
-for counter, symbol in enumerate(df.columns.tolist()):
-    data[symbol + ' weight'] = [bobot[counter] for bobot in p_weights]
-
-portfolios = pd.DataFrame(data)
-print(portfolios.head())
-
-# Plot efficient frontier
-portfolios.plot.scatter(x='Volatility', y='Returns', marker='o', s=10, alpha=0.3, grid=True, figsize=[10,10])
 
 
 
