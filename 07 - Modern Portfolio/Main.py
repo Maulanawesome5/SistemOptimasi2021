4430bf26ff3840ba2cf235ef78318e356e71653a
Maulanawesome5 <maulanawesome5@gmail.com>
2021-12-22 16:49:26 +0700

    Menambah beberapa baris program StockClass.py dan Main.py

--- 07 - Modern Portfolio/Main.py
+++ 07 - Modern Portfolio/Main.py
@@ -1,3 +1,30 @@
+"""
+__________________________________________________________
+Tabel Sektor | Tabel Subsektor | Tabel Saham | Tabel Harga
+__________________________________________________________
+   Property  |   Real Estate   |    APLN     |     123
+   Property  |   Real Estate   |    ASRI     |     163
+   Property  |   Real Estate   |    CTRA     |     990
+   Property  |   Real Estate   |    PWON     |     462
+   Property  |   Real Estate   |    PWON     |     850
+
+Input :
+1. Input Master -> Sektor, Subsektor, Kode Saham (mis. ASII, ACES, AGII...)
+2. Input Investasi
+3. Input Harga Beli
+4. Input Kalkulasi
+5. Transaksi
+
+Output Tabel :
+______________________________________________________________________________
+No. | Ticker Saham |      Nama Saham              | Harga | Bobot | Jumlah Lot
+______________________________________________________________________________
+1.  |    APLN.JK   | PT. Agung Podomoro Land Tbk. |  123  |
+2.  |    ASRI.JK   | PT. Alam Sutera Realty Tbk.  |  163  |
+3.  |    CTRA.JK   | PT. Ciputra Development Tbk. |  990  |
+4.  |    PWON.JK   | PT. Pakuwon Jati Tbk.        |  462  |
+5.  |    SMRA.JK   | PT. Summarecon Agung Tbk.    |  850  |
+"""
 # Import package
 import tkinter as tk
 
--- 07 - Modern Portfolio/StockClass.py
+++ 07 - Modern Portfolio/StockClass.py
@@ -1,10 +1,15 @@
 import pandas_datareader as web
-
+from Saham import dataFrame
 
 """
 __________________________________________________________
 Tabel Sektor | Tabel Subsektor | Tabel Saham | Tabel Harga
 __________________________________________________________
+   Property  |   Real Estate   |    APLN     |     123
+   Property  |   Real Estate   |    ASRI     |     163
+   Property  |   Real Estate   |    CTRA     |     990
+   Property  |   Real Estate   |    PWON     |     462
+   Property  |   Real Estate   |    PWON     |     850
 
 Input :
 1. Input Master -> Sektor, Subsektor, Kode Saham (mis. ASII, ACES, AGII...)
@@ -14,17 +19,26 @@ Input :
 5. Transaksi
 
 Output Tabel :
-____________________________________________________________
-No. | Ticker Saham | Nama Saham | Harga | Bobot | Jumlah Lot
-____________________________________________________________
+______________________________________________________________________________
+No. | Ticker Saham |      Nama Saham              | Harga | Bobot | Jumlah Lot
+______________________________________________________________________________
+1.  |    APLN.JK   | PT. Agung Podomoro Land Tbk. |  123  |
+2.  |    ASRI.JK   | PT. Alam Sutera Realty Tbk.  |  163  |
+3.  |    CTRA.JK   | PT. Ciputra Development Tbk. |  990  |
+4.  |    PWON.JK   | PT. Pakuwon Jati Tbk.        |  462  |
+5.  |    SMRA.JK   | PT. Summarecon Agung Tbk.    |  850  |
 """
 
 class Saham:
-
-    def __init__(self):
-        pass
+    index_nomor = 1
     
-    pass
+    def __init__(self, sektor, subsektor, tickerSaham, hargaSaham):
+        self.__Sektor = sektor
+        self.__SubSektor = subsektor
+        self.__TickerSaham = tickerSaham
+        self.__HargaSaham = hargaSaham
 
+
+apln = Saham()
 
 # End program
