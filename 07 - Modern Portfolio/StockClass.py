6e0fecde931b3f0f7cb6cf03bb49db3e926c4c82
maulanawesome5 <60890352+Maulanawesome5@users.noreply.github.com>
2021-12-22 19:58:56 +0700

    Mengubah StockClass.py
    Menambahkan getter dan class method property. Membuat baris method DisplaySaham.
    Problem :
    Berhasil membuat object instance saham, namun belum bisa ditampilkan menjadi satu tabel menggunakan method DisplaySaham.

--- 07 - Modern Portfolio/StockClass.py
+++ 07 - Modern Portfolio/StockClass.py
@@ -1,5 +1,4 @@
 import pandas_datareader as web
-# from Saham import dataFrame
 from tabulate import tabulate # Pip install tabulate
 
 """
@@ -31,42 +30,62 @@ ______________________________________________________________________________
 """
 
 class Saham:
-    index_nomor = 1
+    index_nomor = 0 #Nomor urut
     
     def __init__(self, sektor, subsektor, tickerSaham, hargaSaham):
         self.__Sektor = sektor
         self.__SubSektor = subsektor
         self.__TickerSaham = tickerSaham
         self.__HargaSaham = hargaSaham
+    
+    @property
+    def SektorEmiten(self):
+        pass
+    
+    @property
+    def SubSektorEmiten(self):
+        pass
+    
+    @property
+    def TickerSaham(self):
+        pass
+    
+    @property
+    def HargaSaham(self):
+        pass
 
+    @SektorEmiten.getter
     def SektorEmiten(self):
         return self.__Sektor
     
+    @SubSektorEmiten.getter
     def SubSektorEmiten(self):
         return self.__SubSektor
     
+    @TickerSaham.getter
     def TickerSaham(self):
         return self.__TickerSaham
     
+    @HargaSaham.getter
     def HargaSaham(self):
         return self.__HargaSaham
-
+    
+    # @classmethod
     def DisplaySaham(self):
         DisplayTable = [['Sektor', 'Subsektor', 'Kode Saham', 'Harga Saham'],
         [self.__Sektor, self.__SubSektor, self.__TickerSaham, self.__HargaSaham]]
+        
         print(tabulate(DisplayTable))
+        # print(self.SektorEmiten, self.SubSektorEmiten, self.TickerSaham, self.HargaSaham)
 
 
-apln = Saham("Property", "Real Estate", "APLN.JK", 123)
-asri = Saham("Property", "Real Estate", "ASRI.JK", 163)
+apln = Saham("Properti", "Real Estate", "APLN.JK", 123)
+asri = Saham("Properti", "Real Estate", "ASRI.JK", 163)
+ctra = Saham("Properti", "Real Estate", "CTRA.JK", 990)
 
-asri.DisplaySaham()
-
-
-
-
-
-
+apln.DisplaySaham()
+asri.DisplaySaham() 
+ctra.DisplaySaham() # Class Method
 
 
 
